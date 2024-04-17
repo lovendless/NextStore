904139a72c3c28adcd0e2f6faed8473bec2138fc
lovendless <i.k.shevery@gmail.com>
2024-04-17 16:49:04 +0700

    allowed origins updated

--- config/allowedOrigins.ts
+++ config/allowedOrigins.ts
@@ -3,6 +3,8 @@ export const allowedOrigins = [
    'http://127.0.0.1:5500',
    'http://localhost:3000',
    'localhost:3000',
+   'https://next-store-ten-sigma.vercel.app',
    'next-store-ten-sigma.vercel.app',
-   'next-store-git-main-lovendless-projects.vercel.app'
+   'next-store-git-main-lovendless-projects.vercel.app',
+   'https://next-store-git-main-lovendless-projects.vercel.app'
 ];
